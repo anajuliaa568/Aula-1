ff837b31b3f84d57ba75a8eefbf0a0a07b919b6a
900065 <900065@GREEN12>
2019-09-18 17:27:39 -0300

    Criação de  classes (quarta feira)

---
+++ MinhaPrimeiraClass/MinhaSegundaClasse/CestaDeFrutas.cs
@@ -0,0 +1,12 @@
+﻿namespace MinhaSegundaClasse
+{
+    internal class CestaDeFrutas
+    {
+        public CestaDeFrutas()
+        {
+        }
+
+        public int uva { get; set; }
+        public int melancia { get; set; }
+    }
+}
---
+++ MinhaPrimeiraClass/MinhaSegundaClasse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaSegundaClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var minhaCestaDeFrutas = new List<CestaDeFrutas>();
+
+            minhaCestaDeFrutas.Add(new CestaDeFrutas()
+            {
+                Nome = "Uva",
+                Quantidade = 5
+
+            });
+
+            {
+                Nome = "Melancia",
+                Quantidade = 2
+
+            });
+
+            minhaCestaDeFrutas.ForEach(i =>
+            Console.WriteLine($"Banana melancia"));
+
+            Console.ReadKey();
+        }
+    }
+    }
+
